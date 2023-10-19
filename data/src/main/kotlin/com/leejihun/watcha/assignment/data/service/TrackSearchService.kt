314278fb8342ee0b8a658f55bb83f5c2fdafb728
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-20 06:54:55 +0900

    limit query 추가

--- data/src/main/kotlin/com/leejihun/watcha/assignment/data/service/TrackSearchService.kt
+++ data/src/main/kotlin/com/leejihun/watcha/assignment/data/service/TrackSearchService.kt
@@ -10,5 +10,6 @@ interface TrackSearchService {
   suspend fun getTrackList(
     @Query("term") term: String = "greenday",
     @Query("entity") entity: String = "song",
+    @Query("limit") limit: Int = 10,
   ): TrackSearchResponse
 }
