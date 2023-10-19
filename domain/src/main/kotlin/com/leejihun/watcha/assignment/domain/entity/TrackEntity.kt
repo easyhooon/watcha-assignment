78fbd1e0ae60e9be2ac1c83ba56234c9098ac608
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-20 00:41:23 +0900

    TrackEntity 및 TrackSearchMapper 추가

---
+++ domain/src/main/kotlin/com/leejihun/watcha/assignment/domain/entity/TrackEntity.kt
@@ -0,0 +1,9 @@
+package com.leejihun.watcha.assignment.domain.entity
+
+data class TrackEntity(
+  val artistName: String,
+  val collectionName: String,
+  val trackName: String,
+  val artworkUrl60: String,
+  val collectionPrice: Double,
+)
