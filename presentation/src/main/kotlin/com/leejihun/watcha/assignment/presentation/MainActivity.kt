dd76fba84cfb08176d49266644d9c930b992be67
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-20 03:30:59 +0900

    PagingAdapter 및 ViewHolder 구성
    화면과 연동

--- presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/MainActivity.kt
+++ presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/MainActivity.kt
@@ -3,13 +3,16 @@ package com.leejihun.watcha.assignment.presentation
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.paging.map
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.leejihun.watcha.assignment.presentation.adapter.TrackAdapter
+import com.leejihun.watcha.assignment.presentation.adapter.TrackLoadStateAdapter
 import com.leejihun.watcha.assignment.presentation.databinding.ActivityMainBinding
 import com.leejihun.watcha.assignment.presentation.extensions.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,16 +21,48 @@ class MainActivity : AppCompatActivity() {
 
   private val viewModel by viewModels<MainViewModel>()
 
+  private val trackAdapter by lazy { TrackAdapter() }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(binding.root)
 
+    initView()
+    initListener()
+    initObserver()
+  }
+
+  private fun initView() {
+    binding.rvMain.apply {
+      addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+      adapter = trackAdapter.withLoadStateFooter(
+        footer = TrackLoadStateAdapter(
+          trackAdapter::retry,
+        ),
+      )
+    }
+  }
+
+  private fun initListener() {
+    trackAdapter.addLoadStateListener { combinedLoadStates ->
+      val loadState = combinedLoadStates.source
+      val isListEmpty = trackAdapter.itemCount < 1 &&
+        loadState.refresh is LoadState.NotLoading &&
+        loadState.append.endOfPaginationReached
+
+      binding.apply {
+        tvMainNoResult.isVisible = isListEmpty
+        tvMainNoResult.isVisible = isListEmpty
+        pbMain.isVisible = loadState.refresh is LoadState.Loading
+      }
+    }
+  }
+
+  private fun initObserver() {
     repeatOnStarted {
       launch {
-        viewModel.trackList.collectLatest { trackList ->
-          trackList.map { track ->
-            Timber.d("$track")
-          }
+        viewModel.trackList.collectLatest {
+          trackAdapter.submitData(it)
         }
       }
     }
--- presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/MainViewModel.kt
+++ presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/MainViewModel.kt
@@ -1,6 +1,8 @@
 package com.leejihun.watcha.assignment.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.leejihun.watcha.assignment.domain.usecase.GetTrackListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,5 +12,5 @@ class MainViewModel @Inject constructor(
   getTrackListUseCase: GetTrackListUseCase,
 ) : ViewModel() {
 
-  val trackList = getTrackListUseCase()
+  val trackList = getTrackListUseCase().cachedIn(viewModelScope)
 }
