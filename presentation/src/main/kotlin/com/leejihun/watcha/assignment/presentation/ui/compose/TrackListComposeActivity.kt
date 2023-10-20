8bdcd94bf7a1a15666db59c99d6695519f14aaaa
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-10-21 01:19:31 +0900

    compose theme 설정
    color, fonts, theme 설정, 사용하지 않는 color 제거

--- presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/ui/compose/TrackListComposeActivity.kt
+++ presentation/src/main/kotlin/com/leejihun/watcha/assignment/presentation/ui/compose/TrackListComposeActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.leejihun.watcha.assignment.presentation.ui.compose.theme.WatchaassignmentTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,7 +22,7 @@ class TrackListComposeActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colorScheme.background,
+          color = Color.White,
         ) {
           Greeting("Android")
         }
